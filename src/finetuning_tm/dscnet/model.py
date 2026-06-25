08f324965747200ac65e15295bdeb9e091421193
Catherine4444 <130481948+Catherine4444@users.noreply.github.com>
2026-06-25 16:30:07 +0200

    5 epoch

--- src/finetuning_tm/dscnet/model.py
+++
@@ -1,6 +0,0 @@
-from finetuning_tm.dscnet. 
-
-
-def build_model(in_channels=3):
-    # designed to receive 1024x1024 images, outputs preds after sigmoid
-    return DinkNet34(num_classes=1, num_channels=in_channels)
